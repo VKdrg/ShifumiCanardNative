fb8ea7aa4b98efb685236a9620fd5cee0d1fb545
Viktor G. <viktor.gouchet@gmail.com>
2026-04-09 11:45:47 +0200

    alllmost finishedddd

--- main.js
+++ main.js
@@ -1,97 +1,115 @@
 // creating elements variables
     // Header
-        const surpriseBtn = document.getElementById('surprise')
-        surpriseBtn.addEventListener('click', () => {
-            // https://confetti.js.org/ => confetti effect on click of orange ducky
-        })
 
     // Wrapper
         // Computer part
-        // Player part
+            let computerGame = document.getElementById('computerGame')
+            let userGame = document.getElementById('userGame')
+            let computerScore = 0
+            let playerScore = 0
+            let scores = document.getElementById('scores')
+            const results = document.getElementById('results')
+            
+            // Player part
             const button1 = document.getElementById('button-rock')
             const button2 = document.getElementById('button-paper')
             const button3 = document.getElementById('button-scissors')
             // Results
             const resultH2 = document.getElementById('resultH2')
             const resultP= document.getElementById('resultP')
-            let computerScore = 0
-            localStorage.setItem("computerScore", computerScore)
-            let playerScore = 0
-            localStorage.setItem("playerScore", playerScore)
-            
-            
-            // prototype
-            /**
-             * Game
-             * @param {String} userTurn 
-             */
-            function Game(userTurn) {
+            let roundResult = document.getElementById('roundResult')
+
+    // Footer
+
+
+// prototype
+
+    /**
+     * Game
+     * @param {String} userTurn 
+     */
+
+    function Game(userTurn) {
+        
+        const keyMatch = {
+            "0": "rock",
+            "1": "paper",
+            "2": "scissors"
+        }
                 
-                const keyMatch = {
-                    "0": "rock",
-                    "1": "paper",
-                    "2": "scissors"
-                }
-                
-                let computerRandom = Math.floor(Math.random() * 3)
-                let computerTurn = keyMatch[computerRandom]
-                
-                let computerGame = document.getElementById('computerGame')
-                if (computerTurn ===  'rock') {
-                    computerGame.classList.toggle('fa-hand-fist')
-                }
-                else if (computerTurn ===  'paper') {
-            computerGame.classList.toggle('fa-hand')
+        let computerRandom = Math.floor(Math.random() * 3)
+        let computerTurn = keyMatch[computerRandom]
+        
+        if (computerTurn ===  'rock') {
+            computerGame.classList.add('fa-hand-fist')
+            computerGame.classList.remove('fa-hand', 'fa-hand-peace')
+        }
+        else if (computerTurn ===  'paper') {
+        computerGame.classList.add('fa-hand')
+        computerGame.classList.remove('fa-hand-peace', 'fa-hand-fist')
         }
         else{
-            computerGame.classList.toggle('fa-hand-peace')
+            computerGame.classList.add('fa-hand-peace')
+            computerGame.classList.remove('fa-hand', 'fa-hand-fist')
         }
         
-        let userGame = document.getElementById('userGame')
         if (userTurn === 'rock') {
-            userGame.classList.toggle('fa-hand-fist')
+            userGame.classList.add('fa-hand-fist')
+            userGame.classList.remove('fa-hand', 'fa-hand-peace')
         }
         else if (userTurn === 'paper') {
-            userGame.classList.toggle('fa-hand')
+            userGame.classList.add('fa-hand')
+            userGame.classList.remove('fa-hand-peace', 'fa-hand-fist')
         }
         else {
-            userGame.classList.toggle('fa-hand-peace')
+            userGame.classList.add('fa-hand-peace')
+            userGame.classList.remove('fa-hand', 'fa-hand-fist')
         }
-        
-        
-        // create empty and append computerTurn icon
-        
+      
         console.log(`Computer : ${computerTurn} - Player : ${userTurn}`)
         
         if ((computerTurn == 'rock' && userTurn == 'paper' )|| (computerTurn == 'paper' && userTurn == 'scissors') || (computerTurn == 'scissors' && userTurn == 'rock')) {
             localStorage.setItem("playerScore", playerScore++)
-            resultH2.textContent = 'You won !'
-            resultP.textContent = 'Big canard for you.'
-            console.log('Player wins the round !')
+            roundResult.textContent = 'Player wins the round !'
         }
         else if (computerTurn == userTurn) {
             localStorage.setItem("playerScore", playerScore++)
             localStorage.setItem("computerScore", computerScore++)
-            resultH2.textContent = 'It\'s a draw!'
-            resultP.textContent = 'Try again to see if you can win.'
-            console.log('That\'s a draw !')
+            roundResult.textContent = 'That\'s a draw !'
         }
         else {
             localStorage.setItem("computerScore", computerScore++)
-            resultH2.textContent = 'You Lost !'
-            resultP.textContent = 'No canard for you.'
-            console.log('Computer wins the round ! Cheh !')
+            roundResult.textContent = 'Computer wins the round ! Cheh !'
+        }
+
+        if (playerScore === 5 || computerScore === 5) {
+            console.log('end')
+            if (playerScore > computerScore) {
+                resultH2.textContent = 'You won !'
+                resultP.textContent = 'Big canard for you.'
+                results.style.display = 'flex'
+            }
+            else if (playerScore < computerScore) {
+                resultH2.textContent = 'You Lost !'
+                resultP.textContent = 'No canard for you.'
+                results.style.display = 'flex'
+            }
+            else {
+                resultH2.textContent = 'It\'s a draw!'
+                resultP.textContent = 'Try again to see if you can win.'
+                results.style.display = 'flex'
+            }
         }
         
-        console.log(`Scores : C: ${computerScore} - P: ${playerScore}`)
+        scores.textContent = `Scores : Computer: ${computerScore} - Player: ${playerScore}`
     }
     
     button1.addEventListener('click', () => { 
-        Game('rock') 
+        Game('rock')
     })
     button2.addEventListener('click', ()=> { 
-        Game('paper') 
+        Game('paper')
     })
     button3.addEventListener('click', () => { 
-        Game('scissors') 
+        Game('scissors')
     })
